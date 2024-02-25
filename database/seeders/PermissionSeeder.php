b41f0b1186ca0f8923f87ab13378be21a1e29311
Vitaliy Semenov <synthex@bk.ru>
2024-02-25 21:58:12 +0600

    feat: add permissions model and table

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $actions = [
+            'viewAny',
+            'view',
+            'create',
+            'update',
+            'delete',
+            'restore',
+            'forceDelete',
+        ];
+
+        $resources = [
+            'user',
+            'restaurant',
+        ];
+
+        collect($resources)
+            ->crossJoin($actions)
+            ->map(fn (array $set): string => join('.', $set))
+            ->each(fn (string $permission) => Permission::create([
+                'name' => $permission,
+            ]));
+    }
+}
