d64933ff200b645b089095fa4cd1b50d930c505b
Vitaliy Semenov <synthex@bk.ru>
2024-02-26 21:08:14 +0600

    feat: implement store restaurant method

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Enums\RoleName;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\StoreRestaurantRequest;
 use App\Models\City;
 use App\Models\Restaurant;
+use App\Models\Role;
+use App\Models\User;
+use DB;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -27,5 +33,28 @@ class RestaurantController extends Controller
         return Inertia::render('Admin/Restaurants/Create', [
             'cities' => City::get(['id', 'name']),
         ]);
+    }
+
+    public function store(StoreRestaurantRequest $request): RedirectResponse
+    {
+        $validated = $request->validated();
+
+        DB::transaction(function () use ($validated) {
+            $user = User::create([
+                'name'     => $validated['owner_name'],
+                'email'    => $validated['email'],
+                'password' => '',
+            ]);
+
+            $user->roles()->sync(Role::where('name', RoleName::VENDOR->value)->first());
+
+            $user->restaurant()->create([
+                'city_id' => $validated['city_id'],
+                'name'    => $validated['restaurant_name'],
+                'address' => $validated['address'],
+            ]);
+        });
+
+        return to_route('admin.restaurants.index');
     }
 }
