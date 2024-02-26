d226438d593985f9330f049129421482f8fbdeb6
Vitaliy Semenov <synthex@bk.ru>
2024-02-26 09:40:51 +0600

    feat: restaurants db schema

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,13 +6,16 @@ namespace App\Models;
 use App\Enums\RoleName;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -48,6 +51,11 @@ class User extends Authenticatable
     public function roles(): BelongsToMany
     {
         return $this->belongsToMany(Role::class);
+    }
+
+    public function restaurant(): HasOne
+    {
+        return $this->hasOne(Restaurant::class, 'owner_id');
     }
 
     public function isAdmin(): bool
