d2582e615a8dba99137dbfb5ff7cbad40561d25e
Vitaliy Semenov <synthex@bk.ru>
2024-02-28 10:17:55 +0600

    feat: implement homepage and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Restaurant;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,18 +16,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
         $this->call([
             PermissionSeeder::class,
             RoleSeeder::class,
             CitySeeder::class,
             UserSeeder::class,
         ]);
+
+        $this->seedDemoRestaurants();
+    }
+
+    public function seedDemoRestaurants()
+    {
+        $products   = Product::factory(7);
+        $categories = Category::factory(5)->has($products);
+        $restaurant = Restaurant::factory()->has($categories);
+
+        User::factory(50)->vendor()->has($restaurant)->create();
     }
 }
